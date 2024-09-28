04e69d48f03a39ff57fbeb1980964f476224ecb0
lovelyANSH2410 <ansh.kush.2410@gmail.com>
2024-09-28 17:07:03 +0530

    New

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const { resolve } = require('path');
 
 const app = express();
-app.use(cors());
 const port = 3000;
+app.use(cors());
 app.use(express.static('static'));
 
 let products = [
